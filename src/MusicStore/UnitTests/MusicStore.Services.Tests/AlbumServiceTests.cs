2fdaeac50fff54f927efe969341dfec3c8c0c55b
Kasun Kodagoda <KasunK@99x.lk>
2020-02-23 15:54:01 +0530

    Added unit tests project

---
+++ src/MusicStore/UnitTests/MusicStore.Services.Tests/AlbumServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MusicStore.Services.Tests
+{
+    public class AlbumServiceTests
+    {
+        [Fact]
+        public void Calling_AllAlbums_Returns_List_Of_Albums()
+        {
+            Assert.True(true);
+        }
+    }
+}
